b458fb5b80734660c1afe89f5eeb68a776cda8b1
Irsyad Fauzan <irsyadfauzangg@gmail.com>
2026-09-19 14:11:55 +0700

    refactor(snippets): add helper utility function (14-11-55)

---
+++ activity/snippets/snippet_2026-09-19T14-11-55.js
@@ -0,0 +1,3 @@
+// Auto-generated helper snippet - 2026-09-19T14:11:55
+// Helper: Formatting currency IDR
+const formatIDR = (val) => new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(val);
